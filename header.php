d74c6a2ba225c02b3d7241f1c795e38b07a2e34a
cassiocpsgit <83365369+cassiocpsgit@users.noreply.github.com>
2023-10-19 15:16:36 -0300

    upd

---
+++ header.php
@@ -0,0 +1,74 @@
+<header id="header" class="header-effect-shrink" data-plugin-options="{'stickyEnabled': true, 'stickyEffect': 'shrink', 'stickyEnableOnBoxed': false, 'stickyEnableOnMobile': false, 'stickyStartAt': 70, 'stickyChangeLogo': false, 'stickyHeaderContainerHeight': 70}">
+				<div class="header-body border-top-0 box-shadow-none">
+					<div class="header-container header-container-md container">
+						<div class="header-row">
+							<div class="header-column">
+								<div class="header-row">
+									<div class="header-logo">
+										<a href="index.html"><img alt="Foto Cassio" width="70" height="70" data-sticky-width="82" data-sticky-height="40" data-sticky-top="0" src="img/foto_oculos_redonda.png"></a>
+									</div>
+								</div>
+							</div>
+							<div class="header-column justify-content-end">
+								<div class="header-row">
+									<div class="header-nav header-nav-line header-nav-bottom-line header-nav-bottom-line-no-transform header-nav-bottom-line-active-text-dark header-nav-bottom-line-effect-1 order-2 order-lg-1">
+										<div class="header-nav-main header-nav-main-square header-nav-main-dropdown-no-borders header-nav-main-effect-2 header-nav-main-sub-effect-1">
+											<nav class="collapse">
+												<ul class="nav nav-pills" id="mainNav">
+													<li class="dropdown">
+														<a class="dropdown-item dropdown-toggle" href="index.html">
+															Home
+														</a>
+													</li>
+													<li class="dropdown">
+														<a class="dropdown-item dropdown-toggle" href="indexjs.html">
+															Projetos
+														</a>													
+														<ul class="dropdown-menu">
+															<li>
+																<a class="dropdown-item" href="indexjs.html">
+																	<p>JavaScript</p>																	
+																</a>
+															</li>
+														</ul>
+													</li>
+													<li class="dropdown">
+														<a class="dropdown-item dropdown-toggle" href="https://cassiocpsgit.github.io/html_css/">
+															Cursos
+														</a>
+													</li>
+													<li class="dropdown">
+														<a class="dropdown-item dropdown-toggle" href="index.html">
+															Contatos
+														</a>
+														<ul class="dropdown-menu">
+															<li>
+																<a class="dropdown-item" href="index.html">
+																	<p>Endereço</p>
+																	<p>Rua Darzan, 68, Santana, São Paulo - SP - Brasil</p>
+																</a>
+															</li>
+															<li>
+																<a class="dropdown-item" href="index.html#demos">
+																	<p>PHONE</p>
+																	<p>+55-12-99679-9346</p>
+																</a>
+															</li>
+															<li>
+																<a class="dropdown-item" href="index.html#demos">
+																	<p>EMAIL</p>
+																	<a href="mailto:mail@example.com">cassiocortezps@gmail.com</a>
+																</a>
+															</li>
+														</ul>
+													</li>
+												</ul>
+											</nav>
+										</div>
+									</div>														
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
