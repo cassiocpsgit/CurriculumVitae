666be67df42742b70ea22073d406f7d2a5099174
cassiocpsgit <83365369+cassiocpsgit@users.noreply.github.com>
2023-10-27 12:28:45 -0300

    update

--- header.php
+++ header.php
@@ -1,74 +1,92 @@
 <header id="header" class="header-effect-shrink" data-plugin-options="{'stickyEnabled': true, 'stickyEffect': 'shrink', 'stickyEnableOnBoxed': false, 'stickyEnableOnMobile': false, 'stickyStartAt': 70, 'stickyChangeLogo': false, 'stickyHeaderContainerHeight': 70}">
-				<div class="header-body border-top-0 box-shadow-none">
-					<div class="header-container header-container-md container">
-						<div class="header-row">
-							<div class="header-column">
-								<div class="header-row">
-									<div class="header-logo">
-										<a href="index.html"><img alt="Foto Cassio" width="70" height="70" data-sticky-width="82" data-sticky-height="40" data-sticky-top="0" src="img/foto_oculos_redonda.png"></a>
-									</div>
-								</div>
+	<div class="header-body border-top-0 box-shadow-none">
+		<div class="header-container header-container-md container">
+			<div class="header-row">
+				<div class="header-column">
+					<div class="header-row">
+						<div class="header-logo">
+							<a href="index.html"><img alt="Foto Cassio" width="70" height="70" data-sticky-width="82" data-sticky-height="40" data-sticky-top="0" src="img/foto_oculos_redonda.png"></a>
+						</div>
+					</div>
+				</div>
+				<div class="header-column justify-content-end">
+					<div class="header-row">
+						<div class="header-nav header-nav-line header-nav-bottom-line header-nav-bottom-line-no-transform header-nav-bottom-line-active-text-dark header-nav-bottom-line-effect-1 order-2 order-lg-1">
+							<div class="header-nav-main header-nav-main-square header-nav-main-dropdown-no-borders header-nav-main-effect-2 header-nav-main-sub-effect-1">
+								<nav class="collapse">
+									<ul class="nav nav-pills" id="mainNav">
+										<li class="dropdown">
+											<a class="dropdown-item dropdown-toggle" href="index.html">
+												Home
+											</a>
+										</li>
+										<li class="dropdown">
+											<a class="dropdown-item dropdown-toggle" href="indexjs.html">
+												Projetos
+											</a>
+											<ul class="dropdown-menu">
+												<li>
+													<a class="dropdown-item" href="indexjs.html">
+														<p>JavaScript</p>
+													</a>
+												</li>
+											</ul>
+										</li>
+										<li class="dropdown">
+											<a class="dropdown-item dropdown-toggle" href="https://cassiocpsgit.github.io/html_css/">
+												Cursos
+											</a>
+										</li>
+										<li class="dropdown">
+											<a class="dropdown-item dropdown-toggle" href="index.html">
+												Contatos
+											</a>
+											<ul class="dropdown-menu">
+												<li>
+													<a class="dropdown-item" href="index.html">
+														<p>Endereço</p>
+														<p>Rua Darzan, 68, Santana, São Paulo - SP - Brasil</p>
+													</a>
+												</li>
+												<li>
+													<a class="dropdown-item" href="index.html#demos">
+														<p>PHONE</p>
+														<p>+55-12-99679-9346</p>
+													</a>
+												</li>
+												<li>
+													<a class="dropdown-item" href="index.html#demos">
+														<p>EMAIL</p>
+														<a href="mailto:mail@example.com">cassiocortezps@gmail.com</a>
+													</a>
+												</li>
+											</ul>
+										</li>
+									</ul>
+								</nav>
 							</div>
-							<div class="header-column justify-content-end">
-								<div class="header-row">
-									<div class="header-nav header-nav-line header-nav-bottom-line header-nav-bottom-line-no-transform header-nav-bottom-line-active-text-dark header-nav-bottom-line-effect-1 order-2 order-lg-1">
-										<div class="header-nav-main header-nav-main-square header-nav-main-dropdown-no-borders header-nav-main-effect-2 header-nav-main-sub-effect-1">
-											<nav class="collapse">
-												<ul class="nav nav-pills" id="mainNav">
-													<li class="dropdown">
-														<a class="dropdown-item dropdown-toggle" href="index.html">
-															Home
-														</a>
-													</li>
-													<li class="dropdown">
-														<a class="dropdown-item dropdown-toggle" href="indexjs.html">
-															Projetos
-														</a>													
-														<ul class="dropdown-menu">
-															<li>
-																<a class="dropdown-item" href="indexjs.html">
-																	<p>JavaScript</p>																	
-																</a>
-															</li>
-														</ul>
-													</li>
-													<li class="dropdown">
-														<a class="dropdown-item dropdown-toggle" href="https://cassiocpsgit.github.io/html_css/">
-															Cursos
-														</a>
-													</li>
-													<li class="dropdown">
-														<a class="dropdown-item dropdown-toggle" href="index.html">
-															Contatos
-														</a>
-														<ul class="dropdown-menu">
-															<li>
-																<a class="dropdown-item" href="index.html">
-																	<p>Endereço</p>
-																	<p>Rua Darzan, 68, Santana, São Paulo - SP - Brasil</p>
-																</a>
-															</li>
-															<li>
-																<a class="dropdown-item" href="index.html#demos">
-																	<p>PHONE</p>
-																	<p>+55-12-99679-9346</p>
-																</a>
-															</li>
-															<li>
-																<a class="dropdown-item" href="index.html#demos">
-																	<p>EMAIL</p>
-																	<a href="mailto:mail@example.com">cassiocortezps@gmail.com</a>
-																</a>
-															</li>
-														</ul>
-													</li>
-												</ul>
-											</nav>
+							<button class="btn header-btn-collapse-nav" data-bs-toggle="collapse" data-bs-target=".header-nav-main nav">
+								<i class="fas fa-bars"></i>
+							</button>
+						</div>
+						<div class="header-nav-features header-nav-features-no-border header-nav-features-lg-show-border order-1 order-lg-2">
+							<div class="header-nav-feature header-nav-features-search d-inline-flex">
+								<a href="#" class="header-nav-features-toggle text-decoration-none" data-focus="headerSearch" aria-label="Search"><i class="fas fa-search header-nav-top-icon"></i></a>
+								<div class="header-nav-features-dropdown header-nav-features-dropdown-mobile-fixed" id="headerTopSearchDropdown">
+									<form role="search" action="page-search-results.html" method="get">
+										<div class="simple-search input-group">
+											<input class="form-control text-1" id="headerSearch" name="q" type="search" value="" placeholder="Search...">
+											<button class="btn" type="submit" aria-label="Search">
+												<i class="fas fa-search header-nav-top-icon text-color-dark"></i>
+											</button>
 										</div>
-									</div>														
+									</form>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</header>
+			</div>
+		</div>
+	</div>
+</header>
